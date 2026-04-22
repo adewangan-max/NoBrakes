f4cb8dfe2f9580732bebf32416e03e7578784401
Ayush Dewangan <ayush.dewangan2109@gmail.com>
2026-04-22 23:20:09 +0530

    wrong email fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="flex min-h-screen items-center justify-center">
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mx-auto"></div>
-          <p className="mt-4">Loading....</p>
+          <p className="mt-4">Loading...</p>
         </div>
       </div>
     );
